33c3b46448da0d4c031494fdae0aa383cc120b1b
leesterr <leereub@gmail.com>
2023-03-13 11:16:14 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+tele_api_token = '5747674742:AAHQTSikW38q3qVl4_q_QkB5rtgxYu7M2A0'
+api_code = '6c0ea5a581611dc337c9ae0b75763b66'
+
+genius = 'aOOzFUV-QgQrSla6-8H0O0ZZygSt-UewyLRewfdp_lbajayUnj5QBygDc34fexJ7'
